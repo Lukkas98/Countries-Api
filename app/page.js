356381977bca50bfb0ffae534dee9fcd2aa933a5
Lukkas98 <lucaspalma988@gmail.com>
2023-11-03 13:05:54 -0300

    landing page change

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Landing Page</h1>
-      <Link href={"/home"}>Enter</Link>
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+      <h1>NOTHING FOR HERE</h1>
+      <Link className="px-2 py-1 bg-red-700 text-white rounded-md" href={"/home"}>Enter</Link>
     </main>
   )
 }
